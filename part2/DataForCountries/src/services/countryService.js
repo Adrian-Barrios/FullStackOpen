7faea692ac889182d86d5862d3570701cbdc6a88
adrianbarrios <adrianbarrioscortez@gmail.com>
2025-06-15 16:22:18 +0200

    Setup Countries Exercise

---
+++ part2/DataForCountries/src/services/countryService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const API_URL = 'https://studies.cs.helsinki.fi/restcountries/api/all';
+
+const getAllCountries = () => {
+  const request = axios.get(API_URL);
+  return request.then(response => response.data);
+}
+
+export default getAllCountries;
