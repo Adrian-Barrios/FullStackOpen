3f224355ac4b4b3f839a8fe20f70e2dc7b3a6cd9
adrianbarrios <adrianbarrioscortez@gmail.com>
2025-06-15 16:31:55 +0200

    Imported countries via API

--- part2/DataForCountries/src/App.jsx
+++ part2/DataForCountries/src/App.jsx
@@ -1,9 +1,16 @@
 import React, {useState, useEffect} from 'react'
-
-
+import countryService from './services/countryService.js'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    countryService
+      .getAllCountries()
+      .then(response => {
+        console.log(response)
+      })
+  }, [])
 
   return (
     <div>Something</div>
--- part2/DataForCountries/src/services/countryService.js
+++ part2/DataForCountries/src/services/countryService.js
@@ -7,4 +7,14 @@ const getAllCountries = () => {
   return request.then(response => response.data);
 }
 
-export default getAllCountries;
+const getCountryByName = (name) => {  
+  const request = axios.get(`${API_URL}/${name}`);
+  return request.then(response => response.data);
+}
+
+
+export default 
+{
+  getAllCountries,
+  getCountryByName
+};
