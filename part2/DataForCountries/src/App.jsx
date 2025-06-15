4e706dd68b0f3c980d8362266955e8b65e3f3bbc
adrianbarrios <adrianbarrioscortez@gmail.com>
2025-06-15 16:46:24 +0200

    Countries Exercise 2.18

--- part2/DataForCountries/src/App.jsx
+++ part2/DataForCountries/src/App.jsx
@@ -1,19 +1,78 @@
 import React, {useState, useEffect} from 'react'
 import countryService from './services/countryService.js'
 
+
+const Display = ({ countries, filterValue }) => {
+  if (filterValue === '') {
+    return(
+      <div>Please look for a country</div>
+    )
+} 
+  const filteredCountries = countries.filter(country =>
+    country.name.common.toLowerCase().includes(filterValue.toLowerCase())
+  )
+  if (filteredCountries.length > 10) {
+    return (
+      <div>Too many matches, specify another filter</div>
+    )
+  } else if (filteredCountries.length === 1){
+    const country = filteredCountries[0];
+    return (
+      <div>
+        <h2>{country.name.common}</h2>
+        <p>Capital: {country.capital}</p>
+        <p>Population: {country.population}</p>
+        <p>Area: {country.area} km²</p>
+        <h3>Languages:</h3>
+        <ul>
+          {Object.values(country.languages).map((language, index) => (
+            <li key={index}>{language}</li>
+          ))}
+        </ul>
+        <img src={country.flags.png} alt={`Flag of ${country.name.common}`} width="200" />
+      </div>
+    )
+  } else if (filteredCountries.length === 0) {
+    return (
+      <div>No countries found</div>
+    )
+  } else {
+    return (
+      <ul>
+        {filteredCountries.map((country, index) => (
+          <li key={index}>{country.name.common}</li>
+        ))}
+      </ul>
+    )
+  }
+
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [allCountries, setAllCountries] = useState([])
+  const [filterValue, setFilterValue] = useState('')
 
   useEffect(() => {
     countryService
       .getAllCountries()
       .then(response => {
         console.log(response)
+        setAllCountries(response)
+      })
+      .catch(error => {
+        console.error('Error fetching countries:', error)
       })
   }, [])
 
+  const handleFilterChange = (event) => {
+    setFilterValue(event.target.value)}
+
   return (
-    <div>Something</div>
+    <div>
+      filter: <input value={filterValue} onChange={handleFilterChange} />
+      <Display countries={allCountries} filterValue={filterValue} />
+
+    </div>
   )
 }
 
