81a2daad8b43d471556b979348df0ac0a65c1c4c
adrianbarrios <adrianbarrioscortez@gmail.com>
2025-07-13 22:28:27 +0200

    Chore(part1): Exercises 1.1 & 1.2

--- part 1/courseinfo/src/App.jsx
+++ part 1/courseinfo/src/App.jsx
@@ -1,8 +1,51 @@
-import { useState } from 'react'
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
 
-function App() {
+const Part = (props) => {
+  return (
+    <p>{props.name} {props.exercises}</p>
+  )
+}
+
+const Total = (props) => {
   return(
-    null
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+  const total = exercises1 + exercises2 + exercises3
+
+  const Content = () => {
+  return (
+    <>
+      <Part name={part1} exercises={exercises1} />
+      <Part name={part2} exercises={exercises2} />
+      <Part name={part3} exercises={exercises3} />
+    </>
+
+
+  )
+}
+
+  return (
+    <>
+    <Header course={course} />
+    <Content />
+    <Total total={total}/>
+    </>
   )
 }
 
