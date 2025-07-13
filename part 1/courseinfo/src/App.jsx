6cbadad37eed150384e6ad32602a561de3602d73
adrianbarrios <adrianbarrioscortez@gmail.com>
2025-07-13 22:42:48 +0200

    Chore(part1): Exercises 1.3-1.5

--- part 1/courseinfo/src/App.jsx
+++ part 1/courseinfo/src/App.jsx
@@ -10,6 +10,17 @@ const Part = (props) => {
   )
 }
 
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises  
+={props.parts[2].exercises} />
+    </div>
+  )
+}
+
 const Total = (props) => {
   return(
     <p>Number of exercises {props.total}</p>
@@ -19,34 +30,31 @@ const Total = (props) => {
 
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-  const total = exercises1 + exercises2 + exercises3
-
-  const Content = () => {
-  return (
-    <>
-      <Part name={part1} exercises={exercises1} />
-      <Part name={part2} exercises={exercises2} />
-      <Part name={part3} exercises={exercises3} />
-    </>
-
-
-  )
-}
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
 
   return (
-    <>
-    <Header course={course} />
-    <Content />
-    <Total total={total}/>
-    </>
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total total={total} />
+    </div>
   )
 }
-
 export default App
