7457f14e89d7f4faeb8da4b544fe83b4256949ee
adrianbarrios <adrianbarrioscortez@gmail.com>
2025-07-14 20:13:04 +0200

    Chore(part1): Exercise 1.6

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -1,11 +1,44 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  return(
+    <h1>{props.header}</h1>
+  )
+}
+
+const Button = ({text, onclick}) => {
+  return(
+    <button onClick={onclick}>{text}</button>
+  )
+}
+
+const Display = ({good, neutral, bad}) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      </div>
+  )
+}
+ 
 const App = () => {
-  const [count, setCount] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleGoodClick = () => setGood(good + 1)
+  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const handleBadClick = () => setBad(bad + 1)
 
   return (
     <div>
-      <p>Test</p>
+      <Header header="give feedback" />
+      <Button text="good" onclick={handleGoodClick} />
+      <Button text="neutral" onclick={handleNeutralClick} />
+      <Button text="bad" onclick={handleBadClick} />
+      <Display good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
