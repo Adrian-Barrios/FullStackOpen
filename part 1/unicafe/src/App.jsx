6aa015ace2c86f4926fb142910882e52bf3e8536
adrianbarrios <adrianbarrioscortez@gmail.com>
2025-07-14 20:40:25 +0200

    Chore(part1): Exercise 1.7

--- part 1/unicafe/src/App.jsx
+++ part 1/unicafe/src/App.jsx
@@ -12,13 +12,16 @@ const Button = ({text, onclick}) => {
   )
 }
 
-const Display = ({good, neutral, bad}) => {
+const Display = ({good, neutral, bad, all, average}) => {
   return (
     <div>
       <h2>Statistics</h2>
       <p>Good: {good}</p>
       <p>Neutral: {neutral}</p>
       <p>Bad: {bad}</p>
+      <p>all: {all}</p>
+      <p>Average: {average.toFixed(2)}</p>
+      <p>Positive:{all==0 ? 0:  (good/all).toFixed(2)}%</p>
       </div>
   )
 }
@@ -27,10 +30,31 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+  const [average, setAverage] = useState(0)
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
-  const handleBadClick = () => setBad(bad + 1)
+  const handleGoodClick = () => {
+    const newGood = good + 1
+    setAll(all + 1)
+    setGood(newGood)
+    const newAverage = (newGood - bad) / (all + 1)
+    setAverage(newAverage)
+  }
+
+  const handleNeutralClick = () => {
+    const newNeutral = neutral + 1
+    setAll(all + 1)
+    setNeutral(newNeutral)
+    const newAverage = (good - bad) / (all + 1)
+    setAverage(newAverage)
+  }
+  const handleBadClick = () => {
+    const newBad = bad + 1
+    setAll(all + 1)
+    setBad(newBad)
+    const newAverage = (good - newBad) / (all + 1)
+    setAverage(newAverage)
+  }
 
   return (
     <div>
@@ -38,7 +62,7 @@ const App = () => {
       <Button text="good" onclick={handleGoodClick} />
       <Button text="neutral" onclick={handleNeutralClick} />
       <Button text="bad" onclick={handleBadClick} />
-      <Display good={good} neutral={neutral} bad={bad} />
+      <Display good={good} neutral={neutral} bad={bad} all={all} average={average}/>
     </div>
   )
 }
