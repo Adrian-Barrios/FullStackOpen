dd7b08e818052394299dbdaa265c77449967bdb4
adrianbarrios <adrianbarrioscortez@gmail.com>
2025-07-15 17:35:34 +0200

    Implemented axios post to notes

--- part 2/notes/src/App.jsx
+++ part 2/notes/src/App.jsx
@@ -17,17 +17,44 @@ const App = () => {
       })
   }
   useEffect(hook, [])
-  console.log('render', notes.length, 'notes')
+
+  const handleNewNote = (event) => {
+    setNewNote(event.target.value)
+  }
+
+  const addNote = event => {
+  event.preventDefault()
+  const noteObject = {
+    content: newNote,
+    important: Math.random() < 0.5,
+  }
+  axios
+    .post('http://localhost:3001/notes', noteObject)
+    .then(response => {
+      console.log(response)
+    })
+    setNewNote('')
+}
+  const notesToShow = showAll ? notes : notes.filter(note => note.important===true)
+
+  const handleFilterChange = () => {
+    setShowAll(!showAll)
+  }
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note =>
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
         )}
-
       </ul>
+      <form>
+        <input value={newNote} onChange={handleNewNote}/>
+        <button type="submit" onClick={addNote}>Save Note</button>
+      </form>
+      <button onClick={handleFilterChange}>
+        Show {showAll ? 'important' : 'all'}</button>
     </div>
   )
 }
