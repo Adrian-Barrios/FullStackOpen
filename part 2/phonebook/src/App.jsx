e32d1ab8e6cd76dc3f121f61388358206c4aff8f
adrianbarrios <adrianbarrioscortez@gmail.com>
2025-07-15 09:06:32 +0200

    Chore(part2): Exercise 2.6

--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -6,18 +6,37 @@ const App = () => {
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    const nameObject = {
+      name: newName
+    }
+    const newPersons = persons.concat(nameObject)
+    setPersons(newPersons)
+    setNewName('')
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={addName}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      <ul>
+        {persons.map((person, index) => 
+          <li key={index}>{person.name}</li>
+        )}
+      </ul>
       
     </div>
   )
